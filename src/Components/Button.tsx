b846392f0e7088351553648d0f3b1bdb50a9bf5f
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2023-03-18 14:03:29 -0400

    button

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 
 export interface ButtonProps {
    label: string;
+   name : string;
    onClick: () => void;
 }
 
-export const Button = (props: ButtonProps) => {
+export const Button = (props: ButtonProps & React.HTMLAttributes<HTMLButtonElement>) => {
    return (<>
       <button {...props} onClick={props.onClick} style={{padding: 5}}>{props.label}</button>
    </>);
