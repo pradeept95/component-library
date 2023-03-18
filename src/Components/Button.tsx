89caf9a4320a8b595551e245d7c8187a26946f07
Pradeep Raj Thapaliya <pradeep.thapaliya95@gmail.com>
2023-03-18 12:57:05 -0400

    prettier setup

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
 export interface ButtonProps {
-    label: string;
-    onClick: () => void;
+   label: string;
+   onClick: () => void;
 }
 
 export const Button = (props: ButtonProps) => {
-    return (
-        <button onClick={props.onClick}>{props.label}</button>
-    );
-}
+   return <button onClick={props.onClick}>{props.label}</button>;
+};
